22088282754f585f48a0275db1fda7d4db5169cf
krishpranav <krisna.pranav@gmail.com>
2021-05-24 12:16:54 +0530

    [+] added flag usage func [+]

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/logrusorgru/aurora"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/logrusorgru/aurora"
+)
 
 var Version string
 
 var au aurora.Aurora
+
+func parseArgs() (string, string, string, bool) {
+	var query, net, ip string
+	var compact, color, bool
+
+	au = aurora.NewAurora(true)
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", au.Bold(os.Args[0]))
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "\n Version: %s\n", au.Bold(Version))
+	}
+}
